7defa7d3cfabb676b7afa94aeb8477a5279fdcfe
luis.lima <luis.lima@ceuma.br>
2025-07-09 08:15:38 -0300

    configurações iniciais

---
+++ src/main/java/br/com/projeta_api/ProjetaApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.projeta_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/projeta_api/ProjetaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projeta_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
