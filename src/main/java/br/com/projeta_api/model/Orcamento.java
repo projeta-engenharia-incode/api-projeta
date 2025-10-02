504ae3b148d10ad712c3092700fd8219c230730f
DioSoares <dioxsoares@gmail.com>
2025-10-02 10:02:03 -0300

    criando entidade orcamento e usuario

---
+++ src/main/java/br/com/projeta_api/model/Orcamento.java
@@ -0,0 +1,79 @@
+package br.com.projeta_api.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "orcamentos")
+@NoArgsConstructor
+@Data
+@AllArgsConstructor
+public class Orcamento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "chamado_id", nullable = false)
+    private Chamados chamados;
+
+    @Column(name = "codigo_orcamento", length = 60)
+    private String codigoOrcamento;
+
+    @Column(name = "status_orcamento", length = 40)
+    private String statusOrcamento;
+
+    @ManyToOne (fetch = FetchType.LAZY)
+    @JoinColumn(name = "orcamentista_id", referencedColumnName = "id")
+    private Usuario orcamentista;
+
+    @Column(name = "nome_orcamento", columnDefinition = "TEXT")
+    private String nomeOrcamento;
+
+    @Column
+    private boolean cronograma;
+
+    @Column(name = "centro_de_custo", length = 60)
+    private String centroDeCusto;
+
+    @Column(name = "data_emissao")
+    private LocalDate dataEmissao;
+
+    @Column(name = "data_revisao")
+    private LocalDate dataRevisao;
+
+    @Column(name = "numero_revisao" )
+    private Integer numeroRevisao;
+
+    @Column(name = "data_aprovacao")
+    private LocalDate dataAprovacao;
+
+    @Column(name = "dias_execucao")
+    private Integer diasExecucao;
+
+    @Column(name = "data_finalizacao")
+    private LocalDate dataFinalizacao;
+
+    @Column(name = "numero_se", length = 60)
+    private String numeroSe;
+
+    @Column(name = "numero_ld", length = 60)
+    private String numeroLd;
+
+    @Column(name = "valor_orcado", precision = 14, scale = 2)
+    private Double valorOrcado;
+
+    @Column(name = "valor_aprovado", precision = 14, scale = 2)
+    private Double valorAprovado;
+
+    @Column(name = "valor_previsto", precision = 14, scale = 2)
+    private Double valorPrevisto;
+
+    @Column(name = "percentual_execucao", precision = 5, scale = 2)
+    private Double percentualExecucao;
+}
