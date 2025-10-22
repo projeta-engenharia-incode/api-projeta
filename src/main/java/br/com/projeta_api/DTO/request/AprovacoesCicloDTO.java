132dea3449dfaf2a5ffe59b5fb58909e0f9519eb
luis.lima <luis.lima@ceuma.br>
2025-10-21 23:35:46 -0300

    ajuste após merge com branch de documentos

--- src/main/java/br/com/projeta_api/DTO/request/AprovacoesCicloDTO.java
+++ src/main/java/br/com/projeta_api/DTO/request/AprovacoesCicloDTO.java
@@ -1,10 +1,5 @@
 package br.com.projeta_api.DTO.request;
 
-import br.com.projeta_api.model.Ciclo;
-import br.com.projeta_api.model.Documentos;
-import jakarta.persistence.Column;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,19 +11,11 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class AprovacoesCicloDTO {
     private Long id;
-    @ManyToOne
-    @JoinColumn(name = "documento_id", nullable = false)
-    private Documentos documento;
-    @ManyToOne
-    @JoinColumn(name = "ciclo_id", nullable = false)
-    private Ciclo ciclo;
-    @Column(name = "data_aprovacao")
+    private Long documentoId; // pega o id do documento
+    private Long cicloId;     // pega o id do ciclo
     private LocalDateTime dataAprovacao;
-    @Column(name = "autorizado_pro", length = 120)
     private String autorizadoPor;
-    @Column(name = "status_aprovacao", length = 40)
     private String statusAprovacao;
-    @Column(name = "observacoes", columnDefinition = "TEXT")
     private String observacoes;
 
 }
--- src/main/java/br/com/projeta_api/service/AprovacoesCicloService.java
+++ src/main/java/br/com/projeta_api/service/AprovacoesCicloService.java
@@ -2,7 +2,11 @@ package br.com.projeta_api.service;
 
 import br.com.projeta_api.DTO.request.AprovacoesCicloDTO;
 import br.com.projeta_api.model.AprovacoesCiclo;
+import br.com.projeta_api.model.Ciclo;
+import br.com.projeta_api.model.Documentos;
 import br.com.projeta_api.repository.AprovacoesCicloRepository;
+import br.com.projeta_api.repository.CicloRepository;
+import br.com.projeta_api.repository.DocumentosRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,19 +16,28 @@ public class AprovacoesCicloService {
 
     private final AprovacoesCicloRepository aprovacoesCicloRepository;
 
+    private final DocumentosRepository  documentosRepository;
 
-    public AprovacoesCicloService(AprovacoesCicloRepository aprovacoesCicloRepository) {
+    private final CicloRepository cicloRepository;
+
+
+    public AprovacoesCicloService(AprovacoesCicloRepository aprovacoesCicloRepository,  DocumentosRepository  documentosRepository, CicloRepository cicloRepository) {
         this.aprovacoesCicloRepository = aprovacoesCicloRepository;
+        this.documentosRepository = documentosRepository;
+        this.cicloRepository = cicloRepository;
     }
 
     public AprovacoesCicloDTO saveAprovacaoCiclo(AprovacoesCicloDTO dto) {
         try {
             AprovacoesCiclo entity = new AprovacoesCiclo();
-            entity.setDocumento_id(dto.getDocumento_id());
-            entity.setCiclo_id(dto.getCiclo_id());
-            entity.setData_aprovacao(dto.getData_aprovacao());
-            entity.setAutorizado_por(dto.getAutorizado_por());
-            entity.setStatus_aprovacao(dto.getStatus_aprovacao());
+
+            Documentos doc = documentosRepository.findById(entity.getDocumentos().getId()).orElseThrow(() -> new RuntimeException("ERRO"));
+            Ciclo ciclo =  cicloRepository.findById(entity.getCiclo().getId()).orElseThrow(() -> new RuntimeException("ERRO"));
+            entity.setDocumentos(doc);
+            entity.setCiclo(ciclo);
+            entity.setData_aprovacao(dto.getDataAprovacao());
+            entity.setAutorizado_por(dto.getAutorizadoPor());
+            entity.setStatus_aprovacao(dto.getStatusAprovacao());
             entity.setObservacoes(dto.getObservacoes());
 
             AprovacoesCiclo savedEntity = aprovacoesCicloRepository.save(entity);
@@ -43,8 +56,8 @@ public class AprovacoesCicloService {
         return entities.stream()
                 .map(ciclo -> new AprovacoesCicloDTO(
                         ciclo.getId(),
-                        ciclo.getDocumento_id(),
-                        ciclo.getCiclo_id(),
+                        ciclo.getDocumentos().getId(),
+                        ciclo.getCiclo().getId(),
                         ciclo.getData_aprovacao(),
                         ciclo.getAutorizado_por(),
                         ciclo.getStatus_aprovacao(),
@@ -59,8 +72,8 @@ public class AprovacoesCicloService {
 
         return new AprovacoesCicloDTO(
                 entity.getId(),
-                entity.getDocumento_id(),
-                entity.getCiclo_id(),
+                entity.getDocumentos().getId(),
+                entity.getCiclo().getId(),
                 entity.getData_aprovacao(),
                 entity.getAutorizado_por(),
                 entity.getStatus_aprovacao(),
@@ -72,11 +85,14 @@ public class AprovacoesCicloService {
         AprovacoesCiclo entity = aprovacoesCicloRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Aprovação de ciclo não encontrada com ID: " + id));
 
-        entity.setDocumento_id(dto.getDocumento_id());
-        entity.setCiclo_id(dto.getCiclo_id());
-        entity.setData_aprovacao(dto.getData_aprovacao());
-        entity.setAutorizado_por(dto.getAutorizado_por());
-        entity.setStatus_aprovacao(dto.getStatus_aprovacao());
+        Documentos docUp = documentosRepository.findById(entity.getDocumentos().getId()).orElseThrow(() -> new RuntimeException("ERRO"));
+        Ciclo cicloUp =  cicloRepository.findById(entity.getCiclo().getId()).orElseThrow(() -> new RuntimeException("ERRO"));
+
+        entity.setDocumentos(docUp);
+        entity.setCiclo(cicloUp);
+        entity.setData_aprovacao(dto.getDataAprovacao());
+        entity.setAutorizado_por(dto.getAutorizadoPor());
+        entity.setStatus_aprovacao(dto.getStatusAprovacao());
         entity.setObservacoes(dto.getObservacoes());
 
         AprovacoesCiclo updated = aprovacoesCicloRepository.save(entity);
